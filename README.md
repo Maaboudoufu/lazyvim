1f7c4a94fe3c6a10dc30856d9d4c991f2ff1789f
Jason Tsao <jason.tsao@maaboudoumei.org>
2026-07-14 11:07:17 +0900

    config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# lazyvim
-my template
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable this to make the background transparent
+      styles = {
+        sidebars = "transparent", -- Enable for file tree/NvimTree transparency
+        floats = "transparent", -- Enable for popups/floating windows transparency
+      },
+    },
+  },
+}
